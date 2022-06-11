735f89fe546246b93f4c3a9ffd5d5f090147be1b
JenJukov <100667752+JenJukov@users.noreply.github.com>
2022-06-10 21:18:37 -0700

    Add files via upload

---
+++ wmad202-2022/src/ca/ciccc/main/WMAD202Driver.java
@@ -0,0 +1,29 @@
+package ca.ciccc.main;
+
+
+import ca.ciccc.assignment4.main.Assignment4Driver;
+import ca.ciccc.assignment5.main.Assignment5Driver;
+import ca.ciccc.assignment6.main.Assignment6Driver;
+import ca.ciccc.assignment7.main.Assignment7Driver;
+import ca.ciccc.assignment8.main.Assignment8Driver;
+import ca.ciccc.assignment9.main.Assignment9Driver;
+import ca.ciccc.wmad202.assignment1.main.Assignment1Driver;
+import ca.ciccc.wmad202.assignment2.main.Assignment2Driver;
+import ca.ciccc.wmad202.assignment3.main.AssignmentDriver3;
+import ca.ciccc.wmad202.lecturer.main.LectureDriver;
+
+public class WMAD202Driver {
+    public static void main(String[] args) {
+      // Assignment1Driver.run();//
+        // Assignment2Driver.run();
+      //  AssignmentDriver3.run();
+      //  Assignment4Driver.run();
+      //  Assignment5Driver.run();
+      //  Assignment6Driver.run();
+      //  Assignment7Driver.run();
+      //  Assignment8Driver.run();
+      //  Assignment9Driver.run();
+      //  LectureDriver.run();
+    }
+
+}
