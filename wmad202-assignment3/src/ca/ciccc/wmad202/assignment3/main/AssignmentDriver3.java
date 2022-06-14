184eea79e0de97d2e0c42b4c754084b89750916a
JenJukov <100667752+JenJukov@users.noreply.github.com>
2022-06-13 19:48:28 -0700

    Add files via upload

---
+++ wmad202-assignment3/src/ca/ciccc/wmad202/assignment3/main/AssignmentDriver3.java
@@ -0,0 +1,11 @@
+package ca.ciccc.wmad202.assignment3.main;
+
+import ca.ciccc.wmad202.assignment3.question2.Question2;
+
+public class AssignmentDriver3 {
+
+    public static void run() {
+        Question2 question2 = new Question2();
+        System.out.println("circle area : "+ question2.circleArea(5,13));
+    }
+}
---
+++ wmad202-assignment3/src/ca/ciccc/wmad202/assignment3/problem1/Problem1.java
@@ -0,0 +1,19 @@
+package ca.ciccc.wmad202.assignment3.problem1;
+
+
+import java.util.Scanner;
+
+
+
+
+
+public class Problem1 {
+
+
+        }
+
+
+
+
+
+
